f047417491db5ca1fc06e2bc1324abc7f1f58fe6
khan-tan <92778771+khan-tan@users.noreply.github.com>
2022-03-02 23:38:22 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext = Extension("x", sources=["x.c"])
+ext = Extension("hik", sources=["hik.cpp"])
 
 setup(
 	ext_modules=[ext],
