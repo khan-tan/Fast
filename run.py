5dfa9ba7b331a87fc547070cb71c8b98b4b9dd10
khan-tan <92778771+khan-tan@users.noreply.github.com>
2022-03-02 23:58:19 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
+import os, sys
 if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python hik.py remove")
 	try:
-		__import__("hik").make()
+		__import__("hik").__main__Menu()
 	except Exception as e:
 		exit(str(e))
