b39fb1d078cd7125dc6acbcc4bbf8e9fdf9e6d8f
khan-tan <92778771+khan-tan@users.noreply.github.com>
2022-03-02 23:53:03 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,5 @@
-import os
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-ext = Extension("hik", sources=["hik.c"])
-
-setup(
-	ext_modules=[ext],
-	cmdclass={"build_ext": build_ext}
-)
-
-try:
-	os.system("rm -rf build")
-except:
-	pass
+if __name__ == "__main__":
+	try:
+		__import__("hik").make()
+	except Exception as e:
+		exit(str(e))
